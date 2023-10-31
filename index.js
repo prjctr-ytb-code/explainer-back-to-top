7e3f51a3e731c873452c8ac07ad89fb01e7f5345
SilentImp <thesilentimp@gmail.com>
2023-10-31 13:35:46 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const SCROLL_GAP = 400;
+
+// show/hide button
+const backToTopHolderEl = document.getElementById("back-to-top-holder");
+
+function handleWindowScroll() {
+  if (!backToTopHolderEl) {
+    return;
+  }
+
+  if (document.documentElement.scrollTop > SCROLL_GAP) {
+    backToTopHolderEl.classList.add("page--back-to-top_visible");
+    return;
+  }
+
+  backToTopHolderEl.classList.remove("page--back-to-top_visible");
+}
+
+window.addEventListener("scroll", handleWindowScroll);
+
+// scroll to top on button click
+const backToTopButtonEl = document.getElementById("back-to-top-button");
+
+function handleButtonBackToTopClick() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+(function () {
+  if (!backToTopButtonEl) {
+    return;
+  }
+
+  backToTopButtonEl.addEventListener("click", handleButtonBackToTopClick);
+})();
